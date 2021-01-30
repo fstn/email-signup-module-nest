4d5648f242a306f7152f101eb30554214d671c08
stephenzambaux <stephen.zambaux@gmail.com>
2021-01-30 11:58:12 -0500

    v1.0.30

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,19 +1,23 @@
 
-import {ExecutionContext, Injectable} from '@nestjs/common';
+import {ExecutionContext, Inject, Injectable, InternalServerErrorException} from '@nestjs/common';
 import {AuthGuard} from '@nestjs/passport';
-import {BaseAuthService} from "../interfaces";
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private authService: BaseAuthService) {
+  constructor(
+      @Inject( "AuthService2")
+      private readonly authService:any) {
     super();
+    if(!authService){
+      throw new InternalServerErrorException("AuthService2 is undefined in JwtAuthGuard")
+    }
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const result: any = await (super.canActivate(context))
+    let result: any = await (super.canActivate(context))
     const request = context.switchToHttp().getRequest();
+    result = result  && this.authService.canActivate(request)
     await this.authService?.setUserOnRequest?.(request)
-
     return result
   }
 
